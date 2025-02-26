80404e7b68d4b2889791749fd89f6dbaee6ce3b5
Shakti Sharma <shaktisharma8318@gmail.com>
2025-02-27 00:10:42 +0530

    TODO fix user_input

--- user_input.c
+++ user_input.c
@@ -6,19 +6,24 @@ int main()
     int age;
     printf("what is your age?\n");
     scanf("%d",&age);
-    printf("you are %d years old\n",age);
     
     char grade;
     printf("what grade did you get?\n");
     scanf("%c",&grade);
-    printf("i got an %c grade\n", grade);
 
     char name[25];
     printf("what is your name?\n");
-    scanf("%s", &name);
-    printf("my name is %s\n", name);
+    scanf("%s" ,name);
 
-    
-    
+    printf("my name is %s\n", name);
+    printf("i got %c grade\n", grade);
+    printf("you are %d years old\n",age);
+
+    char full_name[25];
+
+    printf("what is your full name?");
+    fgets(full_name,25,stdin);
+
+    printf("my name is %s",full_name);
     return 0;
 }
