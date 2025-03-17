3f73ce3b9d0ac43518f530a63f9163916dc2c917
Shakti Sharma <shaktisharma8318@gmail.com>
2025-03-18 00:08:07 +0530

    corrected calculator.c

--- calculator.c
+++ calculator.c
@@ -11,34 +11,31 @@ scanf("%lf",&num1);
 printf("enter num2:\n");
 scanf("%lf",&num2);
 
-double addition;
-double substraction;
-double division;  
-double multiplication;
+double result;
 
 
 printf("enter the operation example adddition(+), substraction(-), division(/),or multiplication(*):\n");
-scanf("%c",&operation);
+scanf(" %c",&operation);
 
 switch (operation){
     case '+':
-        addition = num1 + num2;
-        printf("%lf",addition);
+        result = num1 + num2;
+        printf("%lf",result);
     break;
 
     case '-':
-        substraction= num1- num2;
-        printf("%lf",substraction);
+        result= num1- num2;
+        printf("%lf",result);
     break;
 
-    case '/':
-        printf("%lf", division);
-        division  = num1/ num2;
+    case ('/'):
+        result= num1/num2;
+        printf("%lf", result);
     break;
 
     case '*':
-        printf("%lf",multiplication);
-        multiplication = num1*num2;
+        result= num1*num2;
+        printf("%lf",result);
     break;
 
     default :
